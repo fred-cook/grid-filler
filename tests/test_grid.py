a1ba251a862dee32ad39a097ee934d3a45b900f1
Fred Cook <fred@fredcook.co.uk>
2023-07-12 17:23:10 +0100

    #1 adding more tests

--- tests/test_grid.py
+++ tests/test_grid.py
@@ -4,6 +4,16 @@ import pytest
 from grid_filler.grid import CrosswordGrid
 
 @pytest.fixture
+def simple_grid_string():
+    return ("### ###"
+            "### ###"
+            "### ###"
+            "       "
+            "### ###"
+            "### ###"
+            "### ###")
+
+@pytest.fixture
 def example_grid_string():
     return ("#             #"
             " # # # # # # # "
@@ -37,3 +47,22 @@ class TestGrid:
         full_grid = CrosswordGrid('#' * 225)
         assert len(full_grid.lights) == 0
     
+    def test_rectangular_grid(self):
+        test_shape = (10, 5)
+        test_string = ' ' * test_shape[0] * test_shape[1]
+        test_grid = CrosswordGrid(grid_str=test_string,
+                                  shape=test_shape)
+        assert test_grid.grid.shape == test_shape
+        assert len(test_grid.lights) == sum(test_shape)
+
+    def test_simple_grid(self, simple_grid_string):
+        test_grid = CrosswordGrid(simple_grid_string,
+                                  shape=(7, 7))
+        assert len(test_grid.lights) == 2
+
+    def test_incorrect_shape(self):
+        """
+        If the shape cannot be made exactly from the input string
+        """
+        with pytest.raises(ValueError):
+            _ = CrosswordGrid(' ', (10, 10))
