37d41d7fca1a7041893007aa143db776ec300954
Fred Cook <fred@fredcook.co.uk>
2023-07-12 17:22:33 +0100

    adding a main file for quick testing

---
+++ main.py
@@ -0,0 +1,18 @@
+from grid_filler.grid import CrosswordGrid
+
+grid_str = ("#             #"
+            " # # # # # # # "
+            "       #       "
+            " # # # # # # # "
+            "     #         "
+            " # ##### # ### "
+            "     #         "
+            " # # # # # # # "
+            "         #     "
+            " ### # ##### # "
+            "         #     "
+            " # # # # # # # "
+            "       #       "
+            " # # # # # # # "
+            "#             #")
+xw = CrosswordGrid(grid_str)
