26d4a5443cb39606654dc1f006a8449d7b0b8f94
fred-cook <duverfred@yahoo.co.uk>
2023-07-21 20:28:44 +0100

    adding class with big letters 4 printing the grid big

---
+++ grid_filler/display/big_letters.py
@@ -0,0 +1,158 @@
+import numpy as np
+
+class BigLetters:
+    A = np.array(list('      _     '
+         '     /_\    '
+         '    / _ \   '
+         '   /_/ \_\  '
+         '            ')).reshape(5, -1)
+    
+    B = np.array(list('     ___    '
+         '    | _ )   '
+         '    | _ \   '
+         '    |___/   '
+         '            ')).reshape(5, -1)
+    
+    C = np.array(list('     ___    '
+         '    / __|   '
+         '   | (__    '
+         '    \___|   '
+         '            ')).reshape(5, -1)
+    
+    D = np.array(list('    ___     '
+         '   |   \    '
+         '   | |) |   '
+         '   |___/    '
+         '            ')).reshape(5, -1)
+    
+    E = np.array(list('     ___    '
+         '    | __|   '
+         '    | _|    '
+         '    |___|   '
+         '            ')).reshape(5, -1)
+    
+    F = np.array(list('     ___    '
+         '    | __|   '
+         '    | _|    '
+         '    |_|     '
+         '            ')).reshape(5, -1)
+    
+    G = np.array(list('     ___    '
+         '    / __|   '
+         '   | (_ |   '
+         '    \___|   '
+         '            ')).reshape(5, -1)
+    
+    H = np.array(list('    _  _    '
+         '   | || |   '
+         '   | __ |   '
+         '   |_||_|   '
+         '            ')).reshape(5, -1)
+    
+    I = np.array(list('     ___    '
+         '    |_ _|   '
+         '     | |    '
+         '    |___|   '
+         '            ')).reshape(5, -1)
+    
+    J = np.array(list('       _    '
+         '    _ | |   '
+         '   | || |   '
+         '    \__/    '
+         '            ')).reshape(5, -1)
+    
+    K = np.array(list('    _  __   '
+         '   | |/ /   '
+         '   | \' <    '
+         '   |_|\_\   '
+         '            ')).reshape(5, -1)
+    
+    L = np.array(list('    _       '
+         '   | |      '
+         '   | |__    '
+         '   |____|   '
+         '            ')).reshape(5, -1)
+    
+    M = np.array(list('   __  __   '
+         '  |  \/  |  '
+         '  | |\/| |  '
+         '  |_|  |_|  '
+         '            ')).reshape(5, -1)
+    
+    N = np.array(list('    _  _    '
+         '   | \| |   '
+         '   | .` |   '
+         '   |_|\_|   '
+         '            ')).reshape(5, -1)
+    
+    O = np.array(list('     ___    '
+         '    / _ \   '
+         '   | (_) |  '
+         '    \___/   '
+         '            ')).reshape(5, -1)
+    
+    P = np.array(list('     ___    '
+         '    | _ \   '
+         '    |  _/   '
+         '    |_|     '
+         '            ')).reshape(5, -1)
+    
+    Q = np.array(list('     ___    '
+         '    / _ \   '
+         '   | (_) |  '
+         '    \__\_\  '
+         '            ')).reshape(5, -1)
+    
+    R = np.array(list('     ___    '
+         '    | _ \   '
+         '    |   /   '
+         '    |_|_\   '
+         '            ')).reshape(5, -1)
+    
+    S = np.array(list('     ___    '
+         '    / __|   '
+         '    \__ \   '
+         '    |___/   '
+         '            ')).reshape(5, -1)
+    
+    T = np.array(list('    _____   '
+         '   |_   _|  '
+         '     | |    '
+         '     |_|    '
+         '            ')).reshape(5, -1)
+    
+    U = np.array(list('    _   _   '
+         '   | | | |  '
+         '   | |_| |  '
+         '    \___/   '
+         '            ')).reshape(5, -1)
+    
+    V = np.array(list('   __   __  '
+         '   \ \ / /  '
+         '    \ V /   '
+         '     \_/    '
+         '            ')).reshape(5, -1)
+    
+    W = np.array(list(' __      __ '
+         ' \ \    / / '
+         '  \ \/\/ /  '
+         '   \_/\_/   '
+         '            ')).reshape(5, -1)
+    
+    X = np.array(list('   __  __   '
+         '   \ \/ /   '
+         '    >  <    '
+         '   /_/\_\   '
+         '            ')).reshape(5, -1)
+    
+    Y = np.array(list('   __   __  '
+         '   \ \ / /  '
+         '    \ V /   '
+         '     |_|    '
+         '            ')).reshape(5, -1)
+    
+    Z = np.array(list('     ____   '
+         '    |_  /   '
+         '     / /    '
+         '    /___|   '
+         '            ')).reshape(5, -1)
